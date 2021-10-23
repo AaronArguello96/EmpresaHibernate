b04ff97249ebe2e9677a6f4ffa1bc58d1e2b4d14
AaronArguello96 <aaronargu@gmail.com>
2021-10-23 17:41:30 +0200

    Commit desde develop con las operaciones CRUD hechas

---
+++ src/main/java/dao/DepartamentoDAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import org.hibernate.Session;
+
+import empresaHibernate.Departamento;
+
+public class DepartamentoDAO {
+
+	public static void insertarDepartamento(Session s, Departamento departamento) {
+		
+		s.save(departamento);
+	}
+	
+	public static Departamento getDepartamento(Session s, int id) {
+		return s.get(Departamento.class, id);
+	}
+
+	public static void updateDepartamento(Session s, Departamento departamento) {
+		s.update(departamento);
+	}
+	
+	public static void deleteDepartamento(Session s, Departamento departamento) {
+		s.delete(departamento);
+	}
+
+}
---
+++ src/main/java/dao/EmpleadoDAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import org.hibernate.Session;
+
+import empresaHibernate.Empleado;
+
+public class EmpleadoDAO {
+
+	public static void insertarEmpleado(Session s, Empleado empleado) {
+		
+		s.save(empleado);
+	}
+	
+	public static Empleado getEmpleado(Session s, int id) {
+		return s.get(Empleado.class, id);
+	}
+
+	public static void updateEmpleado(Session s, Empleado empleado) {
+		s.update(empleado);
+	}
+	
+	public static void deleteEmpleado(Session s, Empleado empleado) {
+		s.delete(empleado);
+	}
+}
